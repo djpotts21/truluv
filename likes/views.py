d737c00c391d993a9d7d96f57d0286b9b987adcd
Daniel Potts. <djpotts2891@gmail.com>
2024-03-28 23:07:15 +0000

    Changing response to a dict for likes.

--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Like
+from myprofile.models import Profile
 import geopy.distance
 
 
@@ -29,11 +30,30 @@ def unlike_user(request):
 def view_likes(request):
     user = request.user
     likes = Like.objects.get(user=user).likes.all()
+    liked_users = {}
+    for like in likes:
+        liked_user = like.id
+        profile = Profile.objects.get(user=liked_user)
+        userdata = User.objects.get(id=liked_user)
+        name = userdata.first_name
+        if profile.image1:
+            image1 = profile.image1
+        else:
+            image1 = 'https://i.ibb.co/ssFD4BX/no-image.png'
+        distance = geopy.distance.distance(user.profile.location, profile.location)
+        age = profile.age
+        liked_users = ({
+            'name': name,
+            'distance': distance,
+            'age': age,
+            'image1': image1
+        })
+    
     print(likes.all())
     wholikesme = Like.objects.filter(likes=user)
     return render(request,
                   'likes/likes.html', 
                   context = {
-                      'likes': likes,
+                      'likes': liked_users,
                       'wholikesme': wholikesme
                       })
