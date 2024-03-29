7a613d7d741ef33a12f859dcfb2b4eec6248b314
Daniel Potts. <djpotts2891@gmail.com>
2024-03-29 11:14:09 +0000

    Added loops for wholikesme and whoilike

--- likes/admin.py
+++ likes/admin.py
@@ -4,7 +4,6 @@ from .models import Like
 
 # register Like to admin, but only read-only
 class LikeAdmin(admin.ModelAdmin):
-    readonly_fields = ('user', 'likes')
     verbose_name_plural = 'User Likes'
     display = 'User Likes'
     search_fields = ('user__username', 'likes__username')
--- likes/views.py
+++ likes/views.py
@@ -30,30 +30,54 @@ def unlike_user(request):
 def view_likes(request):
     user = request.user
     likes = Like.objects.get(user=user).likes.all()
-    liked_users = {}
+    liked_users = []  # Initialize liked_users as an empty list
     for like in likes:
         liked_user = like.id
         profile = Profile.objects.get(user=liked_user)
         userdata = User.objects.get(id=liked_user)
         name = userdata.first_name
         if profile.image1:
-            image1 = profile.image1
+            image1 = profile.image1.url
         else:
             image1 = 'https://i.ibb.co/ssFD4BX/no-image.png'
         distance = geopy.distance.distance(user.profile.location, profile.location)
+        distance = round(distance.miles, 2)
         age = profile.age
-        liked_users = ({
+        liked_users.append({  # Use append() method to add liked user details to the list
             'name': name,
             'distance': distance,
             'age': age,
-            'image1': image1
+            'image1': image1,
+            'liked_user_id': liked_user
         })
     
-    print(likes.all())
     wholikesme = Like.objects.filter(likes=user)
+    who_likes_me = []
+    print(wholikesme)
+    for like in wholikesme:
+        print(like)
+        liked_user = like.user
+        profile = Profile.objects.get(user=liked_user)
+        userdata = User.objects.get(id=liked_user.id)
+        name = userdata.first_name
+        if profile.image1:
+            image1 = profile.image1.url
+        else:
+            image1 = 'https://i.ibb.co/ssFD4BX/no-image.png'
+        distance = geopy.distance.distance(user.profile.location, profile.location)
+        distance = round(distance.miles, 2)
+        age = profile.age
+        who_likes_me.append({  # Use append() method to add liked user details to the list
+            'name': name,
+            'distance': distance,
+            'age': age,
+            'image1': image1,
+            'liked_user_id': liked_user.id
+        })
+
     return render(request,
-                  'likes/likes.html', 
-                  context = {
+                  'likes/likes.html',
+                  context={
                       'likes': liked_users,
-                      'wholikesme': wholikesme
+                      'wholikesme': who_likes_me
                       })
