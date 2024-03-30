0c5e81359dad117c980bf5a88344312203865c73
Daniel Potts. <djpotts2891@gmail.com>
2024-03-30 21:27:08 +0000

    Started message system

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender',
+                    'receiver',
+                    'message',
+                    'timestamp',
+                    'read',
+                    'read_at',
+                    'flaged'
+                    )
+    search_fields = ['sender', 'receiver', 'message']
+    list_filter = ['timestamp', 'read', 'read_at', 'flaged']
+    ordering = ['timestamp']
+
+
+admin.site.register(Message, MessageAdmin)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from chat.models import Message
+from myprofile.models import Profile
 
 
-def chat(request):
-    return render(request, 'chat/chat.html')
+def send_message(request):
+    if request.method == 'POST':
+        sender = request.user
+        receiver = request.POST.get('receiver')
+        message = request.POST.get('message')
+        Message.objects.create(sender=sender, receiver=receiver, message=message)
+        return redirect('chat')
+
+
+def get_messages(user):
+    sender = user
+    receiver = user
+    message_data = {}
+    all_messages_list = []
+    messages = Message.objects.filter(
+        sender=sender) | Message.objects.filter(
+            receiver=receiver)
+    print(messages)
+    for message in messages.order_by('timestamp'):
+        sender_profile = Profile.objects.get(user=message.sender)
+        receiver_profile = Profile.objects.get(user=message.receiver)
+        message_data = {
+            'sender': message.sender,
+            'sender_profile': sender_profile,
+            'receiver': message.receiver,
+            'receiver_profile': receiver_profile,
+            'message': message.message,
+            'read': message.read,
+            'read_at': message.read_at,
+            'timestamp': message.timestamp,
+        }
+        all_messages_list.append(message_data)
+    return all_messages_list
+
+
+def view_chat(request):
+    user = request.user
+    all_messages = get_messages(user)
+    return render(request, 'chat/chat.html', {
+        'message_data': all_messages,
+    })
