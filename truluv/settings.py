476814f36c3351553d3141fbd7365a563c1ede2c
Daniel Potts. <djpotts2891@gmail.com>
2024-04-08 11:03:51 +0000

    Update to use PostGres DB

--- truluv/settings.py
+++ truluv/settings.py
@@ -113,11 +113,23 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': os.getenv('TL_DB_NAME'),
+         'USER': os.getenv('TL_DB_USER'),
+         'PASSWORD': os.getenv('TL_DB_PASS'),
+         'HOST': os.getenv('TL_DB_HOST'),
+         'PORT': os.getenv('TL_DB_PORT'),
+     }
 }
 
 
