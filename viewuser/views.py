cc7583a0c011e3d8e4f089da9badba8975f49125
Daniel Potts. <djpotts2891@gmail.com>
2024-04-08 10:45:13 +0000

    Fix for geolocation on viewuser

--- viewuser/views.py
+++ viewuser/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from myprofile.models import Profile
 from django.contrib.auth.models import User
+
 import os
 import requests
 import geopy.distance
@@ -24,34 +25,39 @@ def viewuser(request, user_id):
     user_data['first_name'] = profile_user.username
     user_data['last_name'] = profile_user.last_name
 
-    # Get the latitude and longitude from user_data
-    location = user_data.get('location')
-
-    # Get API Key from OS
-    gmapsapikey = os.environ.get('GOOGLE_MAPS_API_KEY_GEOCODE')
-
-    # Make a request to Google's Reverse Geocoding API
-    url = f'https://maps.googleapis.com/maps/api/geocode/\
-        json?latlng={location}&key={gmapsapikey}'
-    response = requests.get(url)
-    data = response.json()
-    print(data)  # This is the response from the API for debugging
-
-    # Extract the town from the response
-    for component in data['results'][0]['address_components']:
-        if 'postal_town' in component['types']:
-            town = component['long_name']
-            break
+    if user_data['location'] is None:
+        user_data['location'] = '0,0'
     else:
-        town = None
+        # Get the latitude and longitude from user_data
+        location = user_data.get('location')
 
-    # Save the location to user_data as 'location_clean'
-    user_data['location_clean'] = town
+        # Get API Key from OS
+        gmapsapikey = os.environ.get('GOOGLE_MAPS_API_KEY_GEOCODE')
 
-    # Calculate the distance between the requestor and the user
-    distance = geopy.distance.distance(requestor_location, location).miles
-    distance = round(distance, 2)
-    user_data['distance'] = distance
+        # Make a request to Google's Reverse Geocoding API
+        req_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+        url = f'{req_url}{location}&key={gmapsapikey}'
+        print(url)
+        response = requests.get(url)
+
+        data = response.json()
+        print(data)  # This is the response from the API for debugging
+
+        # Extract the town from the response
+        for component in data['results'][0]['address_components']:
+            if 'postal_town' in component['types']:
+                town = component['long_name']
+                break
+        else:
+            town = None
+
+        # Save the location to user_data as 'location_clean'
+        user_data['location_clean'] = town
+
+        # Calculate the distance between the requestor and the user
+        distance = geopy.distance.distance(requestor_location, location).miles
+        distance = round(distance, 2)
+        user_data['distance'] = distance
 
     context = {
         'user': user_data,
